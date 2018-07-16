8851ceb2dce26b3de5760480d37f49293c8b4305
Unknown <theweeryan17@gmail.com>
2018-07-16 11:18:48 -0500

    Lets acctuly make music announce and songnick work shal we

--- src/main/java/stream/flarebot/flarebot/Client.java
+++ src/main/java/stream/flarebot/flarebot/Client.java
@@ -23,6 +23,7 @@ import net.dv8tion.jda.core.utils.SessionControllerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stream.flarebot.flarebot.database.RedisController;
+import stream.flarebot.flarebot.music.PlayerListener;
 import stream.flarebot.flarebot.scheduler.Scheduler;
 
 import javax.annotation.Nonnull;
@@ -171,23 +172,7 @@ public class Client {
                 ? lavalink.getLink(guildId).getPlayer()
                 : new LavaplayerPlayerWrapper(musicManager.getPlayer(guildId).getPlayer());
         tracks.put(guildId, new ArrayList<>());
-        player.addListener(
-                new AudioEventAdapterWrapped() {
-                    @Override
-                    public void onEvent(PlayerEvent event) {
-                        super.onEvent(event);
-                        if(event instanceof TrackEndEvent) {
-                            TrackEndEvent endEvent = (TrackEndEvent) event;
-                            if(endEvent.getReason().equals(AudioTrackEndReason.FINISHED)) {
-                                if(tracks.get(guildId).size() > 0) {
-                                    player.playTrack(tracks.get(guildId).get(0));
-                                    tracks.get(guildId).remove(0);
-                                }
-                            }
-                        }
-                    }
-                }
-        );
+        player.addListener(new PlayerListener(guildId));
         return player;
     }
 
